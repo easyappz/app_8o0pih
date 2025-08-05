33c8d15b03c9928cc87cc21e0e1e25634d8b21ca
IbragimKhaniev <newinterny@gmail.com>
2025-08-06 02:12:05 +0300

    Update files

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Calculator />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
